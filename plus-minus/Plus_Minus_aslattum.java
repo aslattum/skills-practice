bc37bb1786656f16c7aeb71d931e248c2833384c
aslattum <aslattum1220@gmail.com>
2017-09-14 20:09:58 -0400

    Update solution to problem

--- plus-minus/Plus_Minus_aslattum.java
+++ plus-minus/Plus_Minus_aslattum.java
@@ -1,8 +1,25 @@
 public class Plus_Minus_aslattum {
   public static void main(String[] args) {
 
-    int array = [-4, 3, -9, 0, 4, 1];
+    int[] array = {-4, 3, -9, 0, 4, 1};
+    int size = 6;
 
+    double frac_pos = 0.0;
+    double frac_neg = 0.0;
+    double frac_zero = 0.0;
 
+    for (int i = 0; i < size; i++) {
+      if (array[i] > 0) {
+        frac_pos += array[i];
+      } else if (array[i] < 0) {
+        frac_neg += array[i];
+      } else {
+        // element is zero
+        frac_zero += array[i];
+      }
+    }
+    System.out.println(frac_pos/size);
+    System.out.println(frac_neg/size);
+    System.out.println(frac_zero/size);
   }
 }
